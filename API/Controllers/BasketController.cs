00d745bc97a2b46380cce0a653eff4b24aaf9826
Stavros Kekakos <stnkekakos@gmail.com>
2025-06-15 20:13:05 +0300

    End of section 7

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,91 @@
+using System;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class BasketController(StoreContext context) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<BasketDto>> GetBasket()
+    {
+        var basket = await RetrieveBasket();
+
+        if (basket == null) return NoContent();
+
+        return basket.ToDto();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<BasketDto>> AddItemToBasket(int productId, int quantity)
+    {
+        //get basket edo ekane to idio me emena aplos to ekane me method
+        var basket = await RetrieveBasket();
+        //create dikos mou tropos mpakalistikos
+        //if (basket == null)
+        //    basket = new Basket
+        //    {
+        //        BasketId = Guid.NewGuid().ToString()
+        //    };
+        basket ??= CreateBasket();
+        //get product sosto
+        var product = await context.Products.FindAsync(productId);
+        if (product == null) return BadRequest("Problem adding item to basket");
+        //add item to basket sosto
+        basket.AddItem(product, quantity);
+        //save changes sosto
+        var result = await context.SaveChangesAsync() > 0;
+        //dn eixa grapsei to line apo kato
+        if (result) return CreatedAtAction(nameof(GetBasket), basket.ToDto());
+
+        return BadRequest("Problem updating basket");
+    }
+
+    
+
+    [HttpDelete]
+    public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
+    {
+        //get basket
+        var basket = await RetrieveBasket();
+        if (basket == null) return BadRequest("Unable to retrieve basket");
+        //remove the item or reduce its quantity
+        //var product = await context.Products.FindAsync(productId);
+        //if (product == null) return BadRequest("Problem removing item from basket");
+        //ta lines apo pano dn xreiazotan
+        basket.RemoveItem(productId, quantity);
+        //save changes
+        var result = await context.SaveChangesAsync() > 0;
+        if (result) return Ok();
+
+        return BadRequest("Problem updating basket");
+    }
+
+    private Basket CreateBasket()
+    {
+        var basketId = Guid.NewGuid().ToString();
+        var cookieOptions = new CookieOptions
+        {
+            IsEssential = true,
+            Expires = DateTime.UtcNow.AddDays(30),
+            SameSite = SameSiteMode.None, 
+            Secure = true
+        };
+        Response.Cookies.Append("basketId", basketId, cookieOptions);
+        var basket = new Basket { BasketId = basketId };
+        context.Baskets.Add(basket);
+        return basket;
+    }
+
+    private async Task<Basket?> RetrieveBasket()
+    {
+        return await context.Baskets
+            .Include(x => x.Items)
+            .ThenInclude(x => x.Product)
+            .FirstOrDefaultAsync(x => x.BasketId == Request.Cookies["basketId"]);
+    }
+}
